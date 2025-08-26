17fbe06c341b33987cc6f80811277211ae9ffaf0
ATudgey <adamtudgey@hotmail.co.uk>
2025-08-26 16:44:15 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyB6_4lbI_3R_FtuE12Op_hkQ6hG5v3UnLo",
   authDomain: "family-command-centre-eaa58.firebaseapp.com",
   projectId: "family-command-centre-eaa58",
-  storageBucket: "family-command-centre-eaa58.firebasestorage.app",
+  storageBucket: "family-command-centre-eaa58.appspot.com",   // ✅ corrected
   messagingSenderId: "640133340686",
   appId: "1:640133340686:web:23fb06974576272020265f"
 };
 
 export const HOUSEHOLD_ID = "tudgey-home";
+
